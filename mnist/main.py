2eb06b83ee8032342fae617b03f1ff6a9ab821a1
Nik <sibnick@gmail.com>
2023-04-27 01:30:27 +0200

    my sampler v3 - wip

--- mnist/main.py
+++ mnist/main.py
@@ -7,6 +7,101 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 
+from torch.utils.data import Sampler
+from typing import Optional
+import collections
+
+
+class MyRandomSampler(Sampler[int]):
+    r"""Samples elements randomly. If without replacement, then sample from a shuffled dataset.
+    If with replacement, then user can specify :attr:`num_samples` to draw.
+
+    Args:
+        data_source (Dataset): dataset to sample from
+        replacement (bool): samples are drawn on-demand with replacement if ``True``, default=``False``
+        num_samples (int): number of samples to draw, default=`len(dataset)`.
+        generator (Generator): Generator used in sampling.
+    """
+    data_source_len: int
+    replacement: bool
+
+    def __init__(self, data_source_len: int, num_samples: Optional[int] = None, generator=None) -> None:
+        self.data_source_len = data_source_len
+        self._num_samples = num_samples
+        self.generator = generator
+        self.stat_cumsum = None
+        if self.generator is None:
+            seed = int(torch.empty((), dtype=torch.int64).random_().item())
+            self.generator = torch.Generator()
+            self.generator.manual_seed(seed)
+        if not isinstance(self.num_samples, int) or self.num_samples <= 0:
+            raise ValueError("num_samples should be a positive integer "
+                             "value, but got num_samples={}".format(self.num_samples))
+
+    @property
+    def num_samples(self) -> int:
+        # dataset size might change at runtime
+        if self._num_samples is None:
+            return self.data_source_len
+        return self._num_samples
+
+    def __iter__(self):
+        n = self.data_source_len
+        for _ in range(self.num_samples // n):
+            yield from torch.randperm(n, generator=self.generator).tolist()
+        yield from torch.randperm(n, generator=self.generator).tolist()[:self.num_samples % n]
+
+    def __len__(self) -> int:
+        return self.num_samples
+
+
+class MyBatchSampler:
+    r"""Wraps another sampler to yield a mini-batch of indices.
+
+    Args:
+        sampler (Sampler or Iterable): Base sampler. Can be any iterable object
+        batch_size (int): Size of mini-batch.
+        drop_last (bool): If ``True``, the sampler will drop the last batch if
+            its size would be less than ``batch_size``
+
+    Example:
+        >>> list(BatchSampler(SequentialSampler(range(10)), batch_size=3, drop_last=False))
+        [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]]
+        >>> list(BatchSampler(SequentialSampler(range(10)), batch_size=3, drop_last=True))
+        [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    """
+
+    def __init__(self, ds_len: int, batch_size: int, mode=True) -> None:
+        # Since collections.abc.Iterable does not check for `__getitem__`, which
+        # is one way for an object to be an iterable, we don't do an `isinstance`
+        # check here.
+        self.ds_len = ds_len
+        self.mode = mode
+        #self.statistics = torch.zeros(ds_len, requires_grad=False).to(device)
+
+        if not isinstance(batch_size, int) or isinstance(batch_size, bool) or \
+                batch_size <= 0:
+            raise ValueError("batch_size should be a positive integer value, "
+                             "but got batch_size={}".format(batch_size))
+
+        self.sampler = MyRandomSampler(ds_len)
+        self.batch_size = batch_size
+
+    def __iter__(self):
+        sampler_iter = iter(self.sampler)
+        while True:
+            try:
+                self.batch = [next(sampler_iter) for _ in range(self.batch_size)]
+                yield self.batch
+            except StopIteration:
+                break
+
+    def __len__(self) -> int:
+        # Can only be called if self.sampler has __len__ implemented
+        # We cannot enforce this condition, so we turn off typechecking for the
+        # implementation below.
+        # Somewhat related: see NOTE [ Lack of Default `__len__` in Python Abstract Base Classes ]
+        return len(self.sampler) // self.batch_size  # type: ignore[arg-type]
 
 class Net(nn.Module):
     def __init__(self):
@@ -34,21 +129,66 @@ class Net(nn.Module):
         return output
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+class LinearRegression(torch.nn.Module):
+    def __init__(self, n):
+        super(LinearRegression, self).__init__()
+        self.n = n #batch size
+        self.e = 8 #embeding
+        self.w = nn.Parameter(torch.rand((n, self.e), requires_grad=True))
+    def forward(self, x):
+        x = torch.triu(x, diagonal=1)
+        x = x[:, :, None]
+        x = x.repeat(1, 1, self.e)
+        ww = self.w[:, None, :].repeat(1, self.n, 1)
+        w2 = self.w[None, :, :].repeat(self.n, 1, 1)
+        w2 = torch.triu(w2, diagonal=1)
+        c1 = x * ww - w2
+        #c1_loss = torch.sum(c1) + torch.abs(self.n - torch.sum(torch.abs(self.w)))
+        return c1
+
+
+def process_losses(losses, batches):
+    diff = losses[1] - losses[0]
+    samples = torch.cartesian_prod(diff, diff)
+    n = losses[0].shape[0]
+    samples = samples.view(n, n, 2)
+    L = samples[:, :, 0]/samples[:, :, 1]
+    sampler_reg = LinearRegression(n)
+    from torch.autograd import Variable
+    optimizer = torch.optim.SGD(sampler_reg.parameters(), lr=0.01)
+    inputs = Variable(L)
+    for epoch in range(100):
         optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
+        outputs = sampler_reg(inputs)
+        loss = torch.sum(torch.abs(outputs))/(n*n-n)/2 + torch.abs(n - torch.sum(torch.abs(sampler_reg.w)))
         loss.backward()
         optimizer.step()
+        print('epoch {}, loss {}'.format(epoch, loss.item()))
+    print(L)
+    pass
+
+
+def train(args, model, device, train_loader, optimizer, epoch, sampler):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        losses = {}
+        data, target = data.to(device), target.to(device)
+        for i in range(0, 2):
+            optimizer.zero_grad()
+            output = model(data)
+            tmp = F.nll_loss(output, target, reduction='none')
+            losses[i] = tmp.clone().detach().cpu()
+            loss = torch.mean(tmp)
+            loss.backward()
+            optimizer.step()
+        process_losses(losses, sampler.batch)
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
+                       100. * batch_idx / len(train_loader), loss.item()))
             if args.dry_run:
                 break
+    return losses
 
 
 def test(model, device, test_loader):
@@ -125,7 +265,8 @@ def main():
                        transform=transform)
     dataset2 = datasets.MNIST('../data', train=False,
                        transform=transform)
-    train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
+    sampler = MyBatchSampler(len(dataset1), batch_size=args.batch_size)
+    train_loader = torch.utils.data.DataLoader(dataset1, batch_sampler=sampler)#, **train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
     model = Net().to(device)
@@ -133,7 +274,7 @@ def main():
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
+        train(args, model, device, train_loader, optimizer, epoch, sampler)
         test(model, device, test_loader)
         scheduler.step()
 
