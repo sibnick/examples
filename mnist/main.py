83da6424efb91e3414056dd3ac1b40f739d99572
Nik <sibnick@gmail.com>
2023-05-29 21:54:19 +0200

    wip - nothing interesting

--- mnist/main.py
+++ mnist/main.py
@@ -6,24 +6,55 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
+import blurpool
+
+class FCNNet(nn.Module):
+    def __init__(self, batch_size):
+        super(Net, self).__init__()
+        self.ones = nn.Parameter(torch.ones((batch_size, 1, 28, 28)))
+        self.fc1 = nn.Linear(28*28, 512)
+        self.fc15 = nn.Linear(512, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.5)
+
+    def forward(self, x):
+        if self.training:
+            x = self.ones * x
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout1(x)
+        x = self.fc15(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, batch_size):
         super(Net, self).__init__()
+        self.ones = nn.Parameter(torch.ones((batch_size, 1, 28, 28)))
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
         self.fc1 = nn.Linear(9216, 128)
         self.fc2 = nn.Linear(128, 10)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        # self.blur = blurpool.BlurPool(channels=1, filt_size=1, stride=2)
 
     def forward(self, x):
+        if self.training:
+            x = self.ones * x
         x = self.conv1(x)
         x = F.relu(x)
         x = self.conv2(x)
         x = F.relu(x)
-        x = F.max_pool2d(x, 2)
+        x = self.pool(x)
+        # x = self.blur(x)
         x = self.dropout1(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
@@ -34,17 +65,19 @@ class Net(nn.Module):
         return output
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(args, model, device, train_loader, optimizer, grad_stat, epoch, lr):
     model.train()
-    label_smoothing = max(0.01, 0.5 - epoch * 0.05)
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        tmp_loss = F.cross_entropy(output, target, reduction='none', label_smoothing=label_smoothing)
+        tmp_loss = F.cross_entropy(output, target, reduction='none')
         loss = torch.mean(tmp_loss)
         loss.backward()
         optimizer.step()
+        fresh_ones = torch.ones((data.shape[0], 1, 28, 28)).to(device)
+        grad_stat[train_loader.batch_sampler.batch] += (fresh_ones - model.ones)/lr
+        model.ones.data = fresh_ones
         train_loader.batch_sampler.update_stats(tmp_loss.detach(), output, target)
 
         if batch_idx % args.log_interval == 0:
@@ -63,7 +96,7 @@ def test(model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
@@ -130,8 +163,14 @@ def main():
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
         ])
+
+    train_transform=transforms.Compose([
+        transforms.ToTensor(),
+        # transforms.RandomAffine(degrees=0, translate=(2/28.0, 0)),
+        transforms.Normalize((0.1307,), (0.3081,))
+        ])
     dataset1 = datasets.MNIST('../data', train=True, download=True,
-                       transform=transform)
+                       transform=train_transform)
     dataset2 = datasets.MNIST('../data', train=False,
                        transform=transform)
     import stat_sampler
@@ -141,15 +180,15 @@ def main():
     # train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
-    model = Net().to(device)
+    model = Net(args.batch_size).to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
-
-    #scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    grad_stat = torch.zeros(len(dataset1), 1, 28, 28).to(device)
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
+        train(args, model, device, train_loader, optimizer, grad_stat, epoch, scheduler.get_lr()[0])
         test(model, device, test_loader)
-        #scheduler.step()
-        #print("last LR: ", scheduler.get_last_lr())
+        scheduler.step()
+        print("last LR: ", scheduler.get_last_lr())
 
     if args.save_model:
         torch.save(model.state_dict(), "mnist_cnn.pt")
