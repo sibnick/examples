7785879f34de84e4f40f2d2023f74748acef8628
Nik <sibnick@gmail.com>
2023-07-02 23:27:26 +0200

    ignore huge loss#3

--- mnist/mygen.py
+++ mnist/mygen.py
@@ -72,7 +72,7 @@ class DynamicWeightBatchSampler:
             self.count_statistics[self.batch] += 1
             #counts = self.count_statistics[self.batch]
             med = torch.median(loss)
-            huge_loss = loss > med*10
+            huge_loss = loss > med # or just loss > med ???
             loss[huge_loss] = 0
             self.statistics[self.batch] = loss.to(self.device)  # / torch.sqrt(counts)
             self.norm_weights()
